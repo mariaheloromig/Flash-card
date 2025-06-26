c005eb6040fc2f1d1827fda6374dce1c6e009b3c
mariaheloromig <maria.romig@escola.pr.gov.br>
2025-06-25 22:12:57 -0300

    .

---
+++ assets/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Biologia',
+    'O que é mitose?',
+    'Divisão celular que forma duas células idênticas.'
+)
+
+criaCartao(
+    'Biologia',
+    'Para que serve o DNA?',
+    'Armazena as informações genéticas do organismo.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'Processo em que plantas produzem glicose e oxigênio usando luz solar.'
+)
+
+criaCartao(
+    'Bionica',
+    'O que estuda a biônica?',
+    'Imita a natureza para criar tecnologias.'
+)
+
+criaCartao(
+    'Bionica',
+    'Como os olhos humanos inspiram câmeras?',
+    'Pelo foco automático e adaptação à luz.'
+)
+
+criaCartao(
+    'Bionica',
+    'O que é uma prótese biônica?',
+    'Dispositivo que imita membros do corpo com movimentos controlados.'
+)
+
+criaCartao(
+    'Bioquímica',
+    'O que são enzimas?',
+    'Proteínas que aceleram reações químicas.'
+)
+
+criaCartao(
+    'Bioquímica',
+    'Para que serve o ATP?',
+    'É a principal fonte de energia celular.'
+)
+
+criaCartao(
+    'Bioquímica',
+    'O que é um aminoácido?',
+    'Unidade que forma as proteínas.'
+)
